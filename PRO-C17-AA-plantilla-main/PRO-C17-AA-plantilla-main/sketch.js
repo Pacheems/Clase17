f620e4b76720a0619291c680178e0c5a2ec5e7ac
Pacheems <94417715+Pacheems@users.noreply.github.com>
2021-12-15 19:21:05 -0600

    Add files via upload

---
+++ PRO-C17-AA-plantilla-main/PRO-C17-AA-plantilla-main/sketch.js
@@ -0,0 +1,15 @@
+var box1
+function setup() 
+{
+  createCanvas(400, 400);
+  box1 = new Box(6,5,4,3,2,1)
+}
+
+function draw() 
+{
+  background(220);
+ box1.show();
+ box1.moveup();
+ box1.move();
+}
+
